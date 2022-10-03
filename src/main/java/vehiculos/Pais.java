7f1c9a0f0b18962aeb5fa647792f7740e8e21b3b
Scrat1124 <111265350+Scrat112403@users.noreply.github.com>
2022-10-03 01:08:58 -0500

    a

---
+++ src/main/java/vehiculos/Pais.java
@@ -0,0 +1,15 @@
+package vehiculos;
+
+import java.util.ArrayList:
+
+public class Pais {
+    String nombre;
+    public int cantidadVehiculos;
+    private ArrayList<Zona> zona = new ArrayList<Zona>();
+
+    public Pais(String nombre) {
+        this.nombre= nombre;
+    }
+
+    public paisMasVendedor
+}
