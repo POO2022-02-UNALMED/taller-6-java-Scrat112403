065325e4dc5d7407597e34231d50e8c52ab6bf67
Scrat1124 <111265350+Scrat112403@users.noreply.github.com>
2022-10-03 02:10:15 -0500

    A

--- src/test/java/test/vehiculos/Camioneta.java
+++ src/test/java/test/vehiculos/Camioneta.java
@@ -20,11 +20,11 @@ public class Camioneta extends Vehiculo {
 		fabricante.cantidadVehiculos++;
 		fabricante.pais.cantidadVehiculos++;
 
-    public void setVolco(boolean volco) {
-		this.volco = volco;
-	}
-
     public boolean isVolco() {
 		return volco;
+	}
+
+    public void setVolco(boolean volco) {
+		this.volco = volco;
 	}
 }
