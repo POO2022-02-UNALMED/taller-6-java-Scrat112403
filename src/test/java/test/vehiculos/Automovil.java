1406d103bb2a0066beace7b1839165cfb1ec9da6
Scrat1124 <111265350+Scrat112403@users.noreply.github.com>
2022-10-03 01:49:12 -0500

    A

--- src/test/java/test/vehiculos/Automovil.java
+++ src/test/java/test/vehiculos/Automovil.java
@@ -1,17 +1,17 @@
 package vehiculos;
 
 public class Automovil extends Vehiculo {
-    private int puestos;
+    protected int puestos;
     public static int cantidadAutomoviles;
 
     public Automovil() {}
 
-    public Automovil(String placa, int puertas, int velocidadMaxima, String nombre, float precio, float peso, String traccion, Fabricante fabricante, int puestos) {
+    public Automovil(String placa, int puertas, int velocidadMaxima, String nombre, float precio, int peso, String traccion, Fabricante fabricante, int puestos) {
         super(placa, puertas, velocidadMaxima, nombre, precio, peso, traccion, fabricante);
         this.puestos= puestos;
     }
 
-    public static Automovil crearAuto(String placa, int puertas, int velocidadMaxima, String nombre, float precio, float peso, String traccion, Fabricante fabricante) {
+    public static Automovil crearAuto(String placa, int puertas, int velocidadMaxima, String nombre, float precio, int peso, String traccion, Fabricante fabricante) {
 		Automovil auto = new Automovil(placa, 4, 100, nombre, precio, peso, "FWD", fabricante, puestos);
 		cantidadAutomoviles++;
         fabricante.cantidadVehiculos++;
